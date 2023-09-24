11af90557675b08a64c43c80e718fa3f0ea39e03
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-09-24 10:28:41 +0300

    added initial files

---
+++ constants.js
@@ -0,0 +1,7 @@
+const gameTime = 5;
+const firstNames = []; // populate!
+const lastNames = []; // populate!
+const teamA = [];
+const teamB = [];
+const eventEveryXtime = 5;
+const eventTypes = [];
