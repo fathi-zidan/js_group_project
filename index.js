86da20f346d17fee47f812f1932fd22388c5ff8a
fathi <fathizidan52@gmail.com>
2023-09-24 13:16:28 +0300

    update

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const msgBoard = document.getElementById("msgBoard");
 const startBtn = document.getElementById("startBtn");
 const gameTimeNum = document.getElementById("gameTimeNum");
-
 const teamAScore = document.getElementById("teamAScore");
 const teamBScore = document.getElementById("teamBScore");
 const teamScore = document.getElementById("teamScore");
-
 /*
     INSTRUCTIONS
-
     ** game
-
         the game time is taken from the constants.js file
         run time that will start from 0 until the gameTime value.
         foreach 1 game second wait 5 real seconds.
@@ -18,16 +14,13 @@ const teamScore = document.getElementById("teamScore");
         every eventEveryXtime amount of time (while the game is running) - generate random event.
         
     ** events
-
         you should have 8 types of events - for example: playerX scored, playerY committed a foul, timeout, rebounds, assist, ect.
         define the events in eventTypes in constants.js.
         every X amount of time (measure it!) - defined in eventEveryXtime in constants, generate a random event.
         log this event using updateMsg functions, and update the correct data.
         for example:
         if player X scored, update team score, and update player personal score.
-
     ** UI
-
         add at least one animation (ball jumping around, chear team etc).
         add at least one sound effect
     
@@ -37,18 +30,18 @@ const teamScore = document.getElementById("teamScore");
         make the code modular - you can move functions into different files. make sure that the 
         file names are logical and the folder names is logical and nicely organized.
 */
-
 startBtn.addEventListener("click", startGame);
-
 function startGame() {
   updateMsg("start game!");
   let totalTime = 0;
   // start timer, generate random events, update date accordingly until game is done.
+  let currentEventTimer = 0;
   let gameInterval = setInterval(function () {
     if (totalTime < gameTime) {
       // generate events if X amount of time passed from the last event.
       totalTime += 1;
       updateGameTime(totalTime);
+      
     } else {
       clearInterval(gameInterval);
       updateMsg("game over!");
@@ -66,63 +59,47 @@ function startGame() {
   }, 100);
   // when the game time is done - check which team won and generate the message with team name, final score, and top scorrer from the team
 }
-
 function updateMsg(newMsg) {
   msgBoard.innerText = newMsg;
 }
-
 function generateTeams() {
   // run generate player 5 times for each team
   // populate each team array with 5 players each
   generateTeam(teamA, 5);
   generateTeam(teamB, 5);
 }
-
 function generateTeam(teamArray, numOfPLayers) {
   for (let i = 0; i < numOfPLayers; i++) {
     teamArray.push(generatePlayer());
   }
 }
-
 function generatePlayer() {
-    // pick random first name from firstNames array
-  
-    // pick random last name from lastNames array
-    // pick random height from 180-220 cm
-
-    // start each player with 0 points and 0 fouls
-    return {
-        FirstName: (Math.random() * (10)).toFixed(),
-        LastName: (Math.random() * (10)).toFixed(),
-        PlayerHeight: (Math.random() * (220 - 180) + 180).toFixed(),
-        Points: 0,
-        Fouls:0
-    }
+  // pick random first name from firstNames array
+  // pick random last name from lastNames array
+  // pick random height from 180-220 cm
+  // start each player with 0 points and 0 fouls
+  return {
+    FirstName: (Math.random() * 10).toFixed(),
+    LastName: (Math.random() * 10).toFixed(),
+    PlayerHeight: (Math.random() * (220 - 180) + 180).toFixed(),
+    Points: 0,
+    Fouls: 0,
+  };
 }
 function updateGameTime(newTime) {
   gameTimeNum.innerText = newTime;
 }
-
 function updateTeamScoreInUI(team, addPoints) {
     // add correct flow - if team is A - update A score, else - update B score
     if (team === 'A'){
         let teamAScoreNum = parseInt(teamAScore.innerText);
         teamAScoreNum += addPoints;
         teamAScore.innerText = teamAScoreNum;
-
     }else if(team === 'B'){
         let teamBScoreNum = parseInt(teamBScore.innerText);
-    if (team === 'A') {
-        const teamAScoreNum = parseInt(teamAScore.innerText);
-        teamAScoreNum += addPoints;
-        teamAScore.innerText = teamAScoreNum;
-
-    } else if (team === 'B') {
-        const teamBScoreNum = parseInt(teamBScore.innerText);
         teamBScoreNum += addPoints;
         teamBScore.innerText = teamBScoreNum;
-
     }
+    
+    
 }
-}
-
